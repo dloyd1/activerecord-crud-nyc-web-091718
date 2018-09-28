3aecdc9d0e1446b5a4711e58a3deb1bd1ccb9a46
Daniel Loyd <dhloyd@icloud.com>
2018-09-28 15:37:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -101,6 +101,7 @@ def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.delete
+  movie.save
   __
 end
 
