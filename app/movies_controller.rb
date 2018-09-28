3cd70e9454c0eb8a1436a7783b5a3e3475215bd8
Daniel Loyd <dhloyd@icloud.com>
2018-09-28 15:29:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,10 +50,12 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
+  Movie.find_by(id:1)
   __
 end
 
 def can_find_by_multiple_attributes
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
   # Search Values:
   # title == "Title"
   # release_date == 2000
